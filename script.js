8365c9251550d7b2e668a99562328c36cf00d291
joyadas1999 <47679860+joyadas1999@users.noreply.github.com>
2020-05-09 01:40:04 -0400

    updated

--- script.js
+++ script.js
@@ -2,28 +2,19 @@ let mainContatiner = document.getElementById('mainContatiner');
 
 let jsonDatabase = [
   {
-    "title" : "Strawberry Lemonade",
-    "picture_url" : "1.jpg",
-    "price" : "$5.00",
-    "info" : "Made from fresh squeezed Lemons and Strawberry.",
+    "name" : "Tiramisu",
+    "pic" : "tira1.jpg",
+    "detail" : "Cake.",
   },
   {
-    "title" : "Mangonada",
-    "picture_url" : "2.jpg",
-    "price" : "$5.50",
-    "info" : "A fresh mango smoothie with chamoy sauce and chilipowder.",
+    "name" : "Tiramisuu",
+    "pic" : "tira2.jpg",
+    "detail" : "This cake is so fluffy",
   },
   {
-    "title" : "Thai Tea",
-    "picture_url" : "3.jpg",
-    "price" : "$6.50",
-    "info" : "Tastes like flowers in your mouth with a sweet taste.",
-  },
-  {
-    "title" : "Whipped Iced Coffee",
-    "picture_url" : "4.jpg",
-    "price" : "$4.50",
-    "info" : "Hand whisked coffee and cold milk to make a fluffy and refreshing drink.",
+    "name" : "More Tiramisu",
+    "pic" : "tira3.jpg",
+    "detail" : "One bite says it all.",
   }
 ]
 
@@ -33,19 +24,15 @@ function createElement(incomingJSON) {
 
   let imgContainer = document.createElement("IMG");
   imgContainer.classList.add("newImage");
-  imgContainer.src = incomingJSON['picture_url'];
+  imgContainer.src = incomingJSON['pic'];
   newContentElement.appendChild(imgContainer);
 
   let subHeading = document.createElement("H3");
-  subHeading.innerText = incomingJSON['title'];
+  subHeading.innerText = incomingJSON['name'];
   newContentElement.appendChild(subHeading);
 
-  let priceTage = document.createElement("H5");
-  priceTage.innerText = incomingJSON['price'];
-  newContentElement.appendChild(priceTage);
-
   let infoContainer = document.createElement("P");
-  infoContainer.innerText = incomingJSON['info'];
+  infoContainer.innerText = incomingJSON['detail'];
   newContentElement.appendChild(infoContainer);
 
   mainContatiner.appendChild(newContentElement);
